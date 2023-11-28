92436a8173ca0f632f8365bf16daa11426870908
insomniumcode <davi.salazar@duocuc.cl>
2023-11-28 18:40:57 -0300

    firebase

--- blank/src/environments/environment.prod.ts
+++ blank/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig:{
+    apiKey: "AIzaSyAGM82ctQ6yIhot-_OqHNSNVHPLF0dj1Zc",
+    authDomain: "mercado-app-fa7a1.firebaseapp.com",
+    projectId: "mercado-app-fa7a1",
+    storageBucket: "mercado-app-fa7a1.appspot.com",
+    messagingSenderId: "325083097185",
+    appId: "1:325083097185:web:a0efe60b763470d813a220"
+  }
+
 };
